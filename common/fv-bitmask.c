074c32bf9f22c52d7e27f9ab3cb98ec5d2dc1fba
Neil Roberts <bpeeluk@yahoo.co.uk>
2015-06-11 20:54:49 +0100

    common: Add a utility for maintaining an fv_buffer of bits
    This adds utility functions for handling an fv_buffer containing a
    series of bits. The bits are stored grouped in unsigned longs. Sizing
    the buffer ensures that the size is rounded up to the nearest long
    with the padding bits unset. There is a loop macro which can quickly
    iterate over the bits using ffsl.

---
+++ common/fv-bitmask.c
@@ -0,0 +1,66 @@
+/*
+ * Finvenkisto
+ *
+ * Copyright (C) 2015 Neil Roberts
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting documentation, and
+ * that the name of the copyright holders not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  The copyright holders make no representations
+ * about the suitability of this software for any purpose.  It is provided "as
+ * is" without express or implied warranty.
+ *
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
+ * EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THIS SOFTWARE.
+ */
+
+#include "config.h"
+
+#include "fv-bitmask.h"
+
+void
+fv_bitmask_set_length(struct fv_buffer *buffer,
+                      int n_bits)
+{
+        unsigned long *longs;
+
+        /* Round the length up to the nearest long */
+        fv_buffer_set_length(buffer,
+                             (n_bits + FV_BITMASK_BITS_PER_LONG - 1) /
+                             FV_BITMASK_BITS_PER_LONG *
+                             sizeof (unsigned long));
+        /* Make sure that the bits in the trailing byte aren't set so
+         * there's never a bit set that's greater or equal to the
+         * number of bits.
+         */
+        if (n_bits % FV_BITMASK_BITS_PER_LONG) {
+                longs = (unsigned long *) buffer->data;
+                longs[FV_BITMASK_LONG(n_bits)] &= FV_BITMASK_MASK(n_bits) - 1;
+        }
+}
+
+void
+fv_bitmask_or(struct fv_buffer *a,
+              const struct fv_buffer *b)
+{
+        unsigned long *longs_a;
+        const unsigned long *longs_b;
+        int i;
+
+        if (b->length > a->length)
+                fv_buffer_set_length(a, b->length);
+
+        longs_a = (unsigned long *) a->data;
+        longs_b = (const unsigned long *) b->data;
+
+        for (i = 0; i < a->length / sizeof (unsigned long); i++)
+                longs_a[i] |= longs_b[i];
+}
---
+++ common/fv-bitmask.h
@@ -0,0 +1,90 @@
+/*
+ * Finvenkisto
+ *
+ * Copyright (C) 2015 Neil Roberts
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting documentation, and
+ * that the name of the copyright holders not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  The copyright holders make no representations
+ * about the suitability of this software for any purpose.  It is provided "as
+ * is" without express or implied warranty.
+ *
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
+ * EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THIS SOFTWARE.
+ */
+
+#ifndef FV_BITMASK_H
+#define FV_BITMASK_H
+
+#include <limits.h>
+#include <stdbool.h>
+
+#include "fv-buffer.h"
+#include "fv-util.h"
+
+#define FV_BITMASK_BITS_PER_LONG (sizeof (unsigned long) * 8)
+#define FV_BITMASK_MASK(bit) (1UL << ((bit) % FV_BITMASK_BITS_PER_LONG))
+#define FV_BITMASK_LONG(bit) ((bit) / FV_BITMASK_BITS_PER_LONG)
+
+void
+fv_bitmask_set_length(struct fv_buffer *buffer,
+                      int n_bits);
+
+static inline void
+fv_bitmask_set(struct fv_buffer *buffer,
+               int bit,
+               bool value)
+{
+        unsigned long *longs = (unsigned long *) buffer->data;
+        unsigned long mask = FV_BITMASK_MASK(bit);
+
+        if (value)
+                longs[FV_BITMASK_LONG(bit)] |= mask;
+        else
+                longs[FV_BITMASK_LONG(bit)] &= ~mask;
+}
+
+static inline bool
+fv_bitmask_get(struct fv_buffer *buffer,
+               int bit)
+{
+        unsigned long *longs = (unsigned long *) buffer->data;
+
+        return !!(longs[FV_BITMASK_LONG(bit)] & FV_BITMASK_MASK(bit));
+}
+
+void
+fv_bitmask_or(struct fv_buffer *a,
+              const struct fv_buffer *b);
+
+/* To be used like this:
+ *  fv_bitmask_for_each(buffer, bit_num) {
+ *    do_something_with(bit_num);
+ *  }
+ * where bit_num is an integer variable that will be set in turn with
+ * all bits (starting from 0) which are set.
+ * Note that this is a nested loop so break won't work.
+ */
+#define fv_bitmask_for_each(buffer, bit_num)                            \
+        for (int _l = 0;                                                \
+             _l < (buffer)->length / sizeof (unsigned long);            \
+             _l++)                                                      \
+                for (unsigned long _mask = ((const unsigned long *)     \
+                                            (buffer)->data)[_l],        \
+                             _bit_pos;                                  \
+                     _bit_pos = fv_util_ffsl(_mask),                    \
+                             (bit_num) = _bit_pos - 1 +                 \
+                             _l * FV_BITMASK_BITS_PER_LONG,             \
+                             _bit_pos;                                  \
+                     _mask &= (ULONG_MAX - 1) << (_bit_pos - 1))
+
+#endif /* FV_BITMASK_H */
